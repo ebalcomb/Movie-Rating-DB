cdb5a00a90602c94be7f5adf899de9d192843bd2
Hackbright Student <user@mini-004.(none)>
2013-10-29 17:07:43 -0700

    all basic pages exist

--- judgement.py
+++ judgement.py
@@ -31,5 +31,18 @@ def list_users():
     user_list = model.db_session.query(model.User).limit(5).all()
     return render_template("user_list.html", users=user_list)
 
+@app.route("/movies")
+def list_movies():
+    movie_list = model.db_session.query(model.Movies).all()
+    return render_template("movie_list.html", movies=movie_list)
+
+@app.route("/users/<user_id>")
+def user_profile(user_id):
+    return render_template("user_profile.html")
+
+@app.route("/movies/<movie_id>")
+def movie_profile(movie_id):
+    return render_template("movie_profile.html")
+
 if __name__ == "__main__":
     app.run(debug = True)
